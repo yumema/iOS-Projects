720de619efd824d9147ce4ea6224058625998e4e
Marina <yumema@gmail.com>
2015-08-11 22:56:33 -0700

    Creating New Project 'ToDoList' and syncing Xcode Source Control to my github repo.

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Marina Ma on 8/11/15.
+//  Copyright (c) 2015 Marina Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
